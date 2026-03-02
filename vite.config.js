99f33992ecedfeb33a1b8c4dde977339abe35eaf
dahanlapuk <itbamuhammadkamil@gmail.com>
2026-03-02 09:43:14 +0700

    fix bug in vercel.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
